d3afde139bc81ef2828403b407f65c97992d1c78
Jazzmyne <jnixon228@west-mec.org>
2020-01-31 15:25:17 -0700

    Advent of Code 1/31/20

--- 2015/day3.js
+++ 2015/day3.js
@@ -33,60 +33,66 @@ function day3b() {
     var output = document.getElementById("output");
     var y = 0;
     var x = 0;
+    var y2 = 0;
+    var x2 = 0;
+    
     var directions = [];
     santa = ["0 0"];
-    robosanta = [];
+    robosanta = ["0 0"];
 
     directions = input.split("");
 
-    for(i = 0; i < directions.length; i += 2) {
-        santa.push(directions[i]);
-        robosanta.push(directions[i + 1]);
-    }
-    console.log(robosanta);
-    console.log(santa);
+    // for(i = 0; i < directions.length; i ++) {
+    //     if(directions[i]%2 !== 0){
+    //         santa.push(directions[i]);
+    //     } else {
+    //         robosanta.push(directions[i]);
+    //     }
+    // }
+    // no  need to make new arrays, just check for odd and even
 
-    for(i = 0; i < santa.length; i++){
-        var horizontal = [];
-        var vertical = [];
-        if (santa[i] === "^") {
-            y += 1;
-            vertical.push(y);
-        } else if (santa[i] === "v") {
-            y -= 1;
-            vertical.push(y);
+    for(i = 0; i < directions.length; i++){
+        if(i%2 !== 0){
+            var horizontal = [];
+            var vertical = [];
+            if (santa[i] === "^") {
+                y += 1;
+                vertical.push(y);
+            } else if (santa[i] === "v") {
+                y -= 1;
+                vertical.push(y);
+            }
+            if (santa[i] === ">") {
+                x += 1;
+                horizontal.push(x);
+            } else if (santa[i] === "<") {
+                x -= 1;
+                horizontal.push(x);
+            }
+            santa.push(y + " " + x);
         }
-        if (santa[i] === ">") {
-            x += 1;
-            horizontal.push(x);
-        } else if (santa[i] === "<") {
-            x -= 1;
-            horizontal.push(x);
+        else {
+            var horizontal = [];
+            var vertical = [];
+            if (robosanta[i] === "^") {
+                y2 += 1;
+                vertical.push(y);
+            } else if (robosanta[i] === "v") {
+                y2 -= 1;
+                vertical.push(y);
+            }
+            if (robosanta[i] === ">") {
+                x2 += 1;
+                horizontal.push(x);
+            } else if (robosanta[i] === "<") {
+                x2 -= 1;
+                horizontal.push(x);
+            }
+            robosanta.push(y + " " + x);
         }
-        santa.push(y + " " + x);
-    }
-
-    for(i = 0; i < robosanta.length; i++){
-        var horizontal = [];
-        var vertical = [];
-        if (robosanta[i] === "^") {
-            y += 1;
-            vertical.push(y);
-        } else if (robosanta[i] === "v") {
-            y -= 1;
-            vertical.push(y);
-        }
-        if (robosanta[i] === ">") {
-            x += 1;
-            horizontal.push(x);
-        } else if (robosanta[i] === "<") {
-            x -= 1;
-            horizontal.push(x);
-        }
-        robosanta.push(y + " " + x);
-    }
+    }   
 
     uniqueRobosanta = new Set(robosanta); //remove duplicate entries
     uniqueSanta = new Set(santa); //remove duplicate entries
-    output.innerHTML = santa.size + robosanta.size - 1;
+    output.innerHTML = uniqueSanta.size + uniqueRobosanta.size;
 }
