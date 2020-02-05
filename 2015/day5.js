29103f163d683eff84661bfe61aa8add62bd0e93
Jazzmyne <jnixon228@west-mec.org>
2020-02-05 15:24:37 -0700

    Advent of Code 2/5/2020
    Updated function for part 2 on day 5

--- 2015/day5.js
+++ 2015/day5.js
@@ -92,4 +92,13 @@ function day5a() {
 function day5b() {
     var input = document.getElementById("input").value;
     var output = document.getElementById("output");
+
+    // It contains a pair of any two letters that appears at least twice in the string without overlapping, like xyxy (xy) or aabcdefgaa (aa), but not like aaa (aa, but it overlaps).
+    // It contains at least one letter which repeats with exactly one letter between them, like xyx, abcdefeghi (efe), or even aaa.
+    // For example:
+
+    // qjhvhtzxzqqjkmpb is nice because is has a pair that appears twice (qj) and a letter that repeats with exactly one letter between them (zxz).
+    // xxyxx is nice because it has a pair that appears twice and a letter that repeats with one between, even though the letters used by each rule overlap.
+    // uurcxstgmygtbstg is naughty because it has a pair (tg) but no repeat with a single letter between them.
+    // ieodomkazucvgmuy is naughty because it has a repeating letter with one between (odo), but no pair that appears twice.
 }
